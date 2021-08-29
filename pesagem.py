342c90fedebc6894266439a61763058f5aa03490
cleiton <cleiton.leonel@gmail.com>
2021-08-29 15:33:34 -0300

    Adição de novas funcionalidades e popups para notificações do sistema.

--- pesagem.py
+++ pesagem.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 #
 import os
+import io
 import sys
 import time
 import socket
+import subprocess
 import PySimpleGUIQt as sg
-from datetime import datetime
 from pytz import timezone
 from threading import Thread
-from tests.weighing import ProductsController
+from datetime import datetime
 from ctypes import cdll, c_uint
+from PIL import Image, ImageSequence
+from tests.weighing import ProductsController
 
 __version__ = 'beta-001'
 __user__ = 'Cleiton'
@@ -18,8 +21,45 @@ products_list = ['Selecione']
 stock_data = []
 totalizer_data = []
 single_data = []
+
 QT_ENTER_KEY1 = 'special 16777220'
 QT_ENTER_KEY2 = 'special 16777221'
+
+settings = {"printer": "ARGOX_OS-2140"}  # {"printer": "POS80"}
+
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    return output, error
+
+
+def check_printer_status(printer_name):
+    global status_result
+    command = "lpstat -p"
+    process = run_command(command)[0].decode().strip()
+    printers_list = [line for line in process.split('\n') if "Unplugged or turned off" not in line]
+    active_printer = [printer_status for idx, printer_status in enumerate(printers_list)
+                      if printer_name in printer_status][0].split(' ')
+    if "desabilitada" in active_printer or "inativa" in active_printer:
+        print(f"IMPRESSORA {settings['printer']} ESTÁ DESABILITADA OU INATIVA.")
+        status_result = False
+    return status_result
+
+
+def get_printers():
+    command = "lpstat -a | awk '{print $1}'"
+    result = run_command(command)[0].decode().strip()
+    return result.split('\n')
+
+
+def print_file(file_name):
+    search_printers = [p_name for idx, p_name in enumerate(get_printers()) if p_name == settings["printer"]]
+    if len(search_printers) == 1:
+        command = f'cat {file_name} | lpr -P {settings["printer"]}'
+        run_command(command)
+    else:
+        print('IMPRESSORA NÃO EXISTE!!!')
 
 
 def ngrok_session():
@@ -41,13 +81,9 @@ def get_uppercase_state():
     return state
 
 
-def force_uppercase():
+def force_uppercase(start=False):
     command = 0
-    try:
-        state = get_uppercase_state()
-    except:
-        state = 'off'
-    if state == 'off':
+    if start:
         command = 2
     x11 = cdll.LoadLibrary("libX11.so.6")
     display = x11.XOpenDisplay(None)
@@ -111,32 +147,88 @@ class WeighingThread(Thread):
                     if current_weight > 0:
                         result_dict = {
                             "result": True,
-                            "value": f'{current_weight / 1000:.3f}'
+                            "value": f'{current_weight / 1000:.3f}',
+                            "is_server": True
                         }
                     elif cont == 5:
                         result_dict = {
                             "result": False,
-                            "value": f'{0 / 1000:.3f}'
+                            "value": f'{0 / 1000:.3f}',
+                            "is_server": True
                         }
                 except:
-                    print('NENHUM SERVER ENCONTRADO.')
+                    result_dict = {
+                        "result": False,
+                        "value": f'{0 / 1000:.3f}',
+                        "is_server": False
+                    }
                     break
                 time.sleep(0.1)
             cont += 1
 
 
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+def get_img_bytes(img_bytes, extension=None):
+    if not extension:
+        extension = 'PNG'
+    bio = io.BytesIO()
+    img_bytes.save(bio, format=extension)
+    return bio.getvalue()
+
+
+def get_img_frames(filename):
+    gif_filename = resource_path(filename)
+    sequence_frames = [get_img_bytes(img.convert('RGBA')) for img in ImageSequence.Iterator(Image.open(gif_filename))]
+    frame_duration = Image.open(gif_filename).info['duration']
+    return sequence_frames, frame_duration
+
+
+def animation_image(window, file_name, text=''):
+    sequence, duration = get_img_frames(file_name)
+    idx = 0
+    while idx in range(2):
+        for frame in sequence:
+            window.read(timeout=duration)
+            window['loading'].update(data=frame)
+            window['p_text'].update(text)
+        idx += 1
+
+
+def printer(location):
+    printer_layout = [[sg.Text('', font=("verdana", 14), justification='center', key="p_text")],
+                      [sg.Column([[sg.Frame('', layout=[[sg.Image(key='loading')]],
+                                            element_justification='center')]])]]
+    win_ = sg.Window('Melinux', printer_layout, location=location, keep_on_top=True, size=(400, 300))
+    status = check_printer_status(settings['printer'])
+    if status:
+        Thread(target=long_operation_thread, args=(print_file,
+                                                   os.path.join(".", "printers/file.txt"),), daemon=True).start()
+        animation_image(win_, 'src/images/printer_mine.gif', 'Imprimindo, aguarde...')
+    else:
+        animation_image(win_, 'src/images/balloon.gif', 'Impressora não encontrada!!!')
+    for i in range(5):
+        _event, _values = win_.read(1)
+        if _event == sg.WIN_CLOSED:
+            break
+    win_.close()
+
+
 def progress_bar(location):
     bar_layout = [[sg.Text('Salvando no estoque...', font=("verdana", 14), justification='center')],
                   [sg.Column([[sg.Frame('', layout=[
-                      [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='progbar')]],
+                      [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='progress_bar')]],
                                         element_justification='center')],
                               [sg.Frame('', layout=[[sg.Cancel(size=(8, 1.2))]], element_justification='center')]])]]
     win_ = sg.Window('Melinux', bar_layout, location=location, keep_on_top=True)
     for i in range(1000):
         _event, _values = win_.read(timeout=1)
-        if event == 'Cancel' or _event == sg.WIN_CLOSED:
+        if _event == 'Cancel' or _event == sg.WIN_CLOSED:
             break
-        win_['progbar'].update_bar(i + 1)
+        win_['progress_bar'].update_bar(i + 1)
     win_.close()
 
 
@@ -144,11 +236,11 @@ def my_popup(location):
     global event_call
     button_left_frame = [
         [sg.Button("Individual", key='single', size=(8, 1.2), font=("verdana", 10),
-                   button_color=('write', 'lightblue'))]
+                   button_color=('black', '#5f6d8c'))]
     ]
     button_right_frame = [
         [sg.Button("Totalizada", key='totalize', size=(8, 1.2), font=("verdana", 10),
-                   button_color=('write', 'lightblue'))]
+                   button_color=('black', '#5f6d8c'))]
     ]
     popup_layout = [
         [sg.Text("Selecione o tipo de pesagem", justification='center', font=("verdana", 14))],
@@ -176,6 +268,30 @@ def my_popup(location):
     win.close()
 
 
+def event_manager():
+    global disable_popup
+    global text_toggle_button
+    if values['selected'] != 'Selecione' and values['selected'] != '':
+        window['product_selected'].update(values['selected'])
+        if not disable_popup:
+            window.disable()
+            window['print'].update(disabled=False, button_color=('black', '#5f6d8c'))
+            my_popup(location=(width // 2 + y - 150, height // 2 + x - 100))
+            # print('OLHA AI O TIPO DE PESAGEM:', event_call)
+            if event_call['event'] == 'totalize':
+                text_toggle_button = 'Totalizada'
+            if event_call['event'] == 'single':
+                text_toggle_button = 'Individual'
+            if event_call['check']:
+                disable_popup = True
+                window['print'].update(disabled=True, button_color=('black', 'grey'))
+            window.enable()
+    if values['selected'] == 'Selecione' and values['selected'] != '':
+        window['product_selected'].update('')
+    products_list.clear()
+    window.Refresh()
+
+
 def welcome_layout():
     global screen_size
     menu_def = [['File', ['Open', 'Save', 'Exit', 'Properties']],
@@ -199,7 +315,7 @@ def welcome_layout():
               ]],
                   element_justification='center', background_color='grey')],
 
-        [sg.Frame('', layout=[[sg.Text('KG COM 1.000', font=('verdana', 20))]], element_justification='left'),
+        [sg.Frame('', layout=[[sg.Text('COM 1.000 KG', font=('verdana', 20))]], element_justification='left'),
          sg.Frame('', layout=[[sg.Text(f'{dias} DIAS', font=('verdana', 20))]], element_justification='center'),
          sg.Frame('', layout=[[sg.Text(f'{acceptable_range}', font=('verdana', 20))]], element_justification='center')],
     ]
@@ -221,20 +337,25 @@ def welcome_layout():
 
     ]
 
-    # SEPARAR DEPOIS....
     combo_frame = [
         [sg.Text('\n' * 3)],
         [sg.Combo(default_value='Selecione', values=products_list, visible_items=6, font=('', 18), text_color='black',
                   change_submits=True, size_px=(600, 60), key='selected', pad=(0, 0), enable_events=True,
                   auto_complete=True, background_color='lightgrey'),
-         # sg.Text(' '), sg.Button('Individual', size=(10, 1.5), key='toggle', enable_events=True, font=("verdana", 14)),
+         # sg.Text(' '), sg.Button('Individual', size=(10, 1.5), key='toggle',
+         # enable_events=True, font=("verdana", 14)),
          # sg.Text(' '),
-         sg.Text(' '), sg.Button('Imprimir', key='print', size=(10, 1.5), enable_events=True, font=("verdana", 14),
-                                 button_color=('write', 'lightblue'), disabled=True),
-         sg.Text(' '), sg.Button('Estocar', key='stock', size=(10, 1.5), enable_events=True, font=("verdana", 14),
-                                 button_color=('write', 'lightblue'), disabled=True),
-         sg.Text(' '), sg.Button('Sair', key='exit', size=(10, 1.5), font=("verdana", 14),
-                                 button_color=('write', 'lightblue')), sg.Text(' ')],
+         ],
+    ]
+
+    buttons_frame = [
+        [sg.Text('\n' * 3)],
+        [sg.Text(' '), sg.Button('Imprimir', key='print', size=(12, 1.5), enable_events=True, font=("verdana", 14),
+                                 button_color=('black', 'grey'), disabled=True),
+         sg.Text(' '), sg.Button('Estocar', key='stock', size=(12, 1.5), enable_events=True, font=("verdana", 14),
+                                 button_color=('black', 'grey'), disabled=True),
+         sg.Text(' '), sg.Button('Sair', key='exit', size=(12, 1.5), font=("verdana", 14),
+                                 button_color=('black', '#5f6d8c')), sg.Text(' ')]
     ]
 
     footer_frame = [
@@ -255,7 +376,7 @@ def welcome_layout():
         [sg.Column([[sg.Frame('', layout=weight_frame, element_justification='left'),
                      sg.Frame('', layout=info_frame)]])],
         [sg.Text(' \n' * 17)] if screen_size[0] == 1920 else '',
-        [sg.Column([[sg.Frame('', layout=combo_frame)]])],  # TENHO QUE SEPARAR DEPOIS...
+        [sg.Column([[sg.Frame('', layout=combo_frame), sg.Frame('', layout=buttons_frame)]])],
         [sg.Column(layout=footer_frame, background_color='lightblue')],
     ]
     return home_layout
@@ -278,6 +399,7 @@ if __name__ == '__main__':
     screen_size = sg.Window('', alpha_channel=0).get_screen_dimensions()
     result_dict = {}
     event_call = {}
+    status_result = True
     host = 'localhost'  # '2.tcp.ngrok.io'  # 'localhost'  # '192.168.1.154'
     port = 3333
     ngrok_session()
@@ -297,9 +419,13 @@ if __name__ == '__main__':
     weight_thread.ip = host
     weight_thread.port = port
     weight_thread.start()
+    if result_dict.get("is_server"):
+        print(result_dict)
+    else:
+        print('Não é um servidor, vou executar a leitura local...')
     weight_task = Thread(target=long_operation_thread, args=(get_products,), daemon=True)
     weight_task.start()
-    force_uppercase()
+    force_uppercase(True)
     current_layout = welcome_layout()
     window = create_window(current_layout, title='Pesagem')
     # screen_size = window.get_screen_dimensions()
@@ -329,19 +455,19 @@ if __name__ == '__main__':
             weight_thread.stop()
             weight_thread.join()
             break
-        if event == 'About...':
+        elif event == 'About...':
             # window.disappear()
             sg.popup('Sobre o Programa', __version__,
                      'Melinux', __version__, location=(width // 2 + y - 100, height // 2 + x - 50),
                      grab_anywhere=True, keep_on_top=True, background_color="grey")
             window.reappear()
-        if event == 'Open':
+        elif event == 'Open':
             filename = sg.popup_get_file('file to open', no_window=True)
             print(filename)
-        if event == 'Properties':
+        elif event == 'Properties':
             print(event)
-            # second_window()
 
+            # second_window()
             ##################################### USAR MAIS TARDE ################################
             """if event == 'toggle':
                 down = not down
@@ -375,36 +501,24 @@ if __name__ == '__main__':
             # if values['selected'] not in 'abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ':
             # window['selected'].update('')
 
-        if event == 'selected':
-            if values['selected'] != 'Selecione' and values['selected'] != '':
-                window['product_selected'].update(values['selected'])
-                window['stock'].update(disabled=False)
-                window['print'].update(disabled=False)
-                if not disable_popup:
-                    window.disable()
-                    my_popup(location=(width // 2 + y - 150, height // 2 + x - 100))
-                    print('OLHA AI O TIPO DE PESAGEM:', event_call)
-                    if event_call['event'] == 'totalize':
-                        text_toggle_button = 'Totalizada'
-                    if event_call['event'] == 'single':
-                        text_toggle_button = 'Individual'
-                    if event_call['check']:
-                        disable_popup = True
-                        window['print'].update(disabled=True)
-                    window.enable()
-            elif values['selected'] == 'Selecione' and values['selected'] != '':
-                window['product_selected'].update('')
-            products_list.clear()
-            window.Refresh()
+        if event == QT_ENTER_KEY1:
+            event_manager()
+        elif event == 'selected':
+            event_manager()
         if result_dict.get('result') and values['selected'] != 'Selecione':
             window['weight'].update(f"{result_dict['value']} KG")
             window.Refresh()
-        if result_dict.get('value') and float(0.600) < float(result_dict['value']) < float(0.800) \
+        if event_call.get('check') and values['selected'] != 'Selecione' and result_dict.get('value') and float(
+                0.600) < float(result_dict['value']) < float(0.800) \
                 and result_dict['value'] != last_weight:
-            print('VOU IMPRIMIR SAPORRA...')
             event = 'print'
         if event == 'print':
-            print('VOU IMPRIMIR A ETIQUETA')
+            window.disable()
+            printer(location=(width // 2 + y - 150, height // 2 + x - 100))
+            window.enable()
+            if status_result:
+                window['stock'].update(disabled=False, button_color=('black', '#5f6d8c'))
+            # print(result_dict['value'])
             # if len(window['selected'].Values) > 0:
             # window['selected'].update(set_to_index=0)
             # values['selected'] = 'Selecione'
@@ -417,17 +531,25 @@ if __name__ == '__main__':
                 # window['toggle'].update('Individual')
                 # down = True
             window.Refresh()
+            # time.sleep(5)
         if event == 'stock':
+            window.disable()
             progress_bar(location=(width // 2 + y - 150, height // 2 + x - 100))
+            window.enable()
             disable_popup = False
             window['selected'].update(set_to_index=0)
             window['product_selected'].update('')
             # window['print'].update(disabled=False)
-            window['stock'].update(disabled=True)
-            window['print'].update(disabled=True)
+            window['stock'].update(disabled=True, button_color=('black', 'grey'))
+            window['print'].update(disabled=True, button_color=('black', 'grey'))
             window['weight'].update(f'{0 / 1000:.3f} KG')
         time.sleep(0.1)
-
+        try:
+            state = get_uppercase_state()
+        except:
+            state = 'off'
+        if state == 'off':
+            force_uppercase(True)
     window.close()
 
 quit()
